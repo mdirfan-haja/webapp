680198180f38b0f341d55787167da1509af624bf
Irfan <53381250+mdirfan-haja@users.noreply.github.com>
2020-02-15 21:39:10 +0000

    js/sass update
    few update on js if statement Booleans

--- script.js
+++ script.js
@@ -7,25 +7,63 @@
 //var pageLoad = true
 
 
+
 function validateForm() {
-    var name1 = document.forms["contactSubmit"]['fname', 'lname', 'subject1'].value
-    var namelen = Number(name1.length)
+    var name1 = document.forms["contactSubmit"]['fname'].value
+    var lastName1 = document.forms["contactSubmit"]['lname'].value
+    var subject1 = document.forms["contactSubmit"]['subject1'].value
+        //var namelen = Number(name1.length)
+        //var fname1 = document.getElementById('fname')
     var errorP = document.getElementById('error1')
     var errorName1 = document.getElementById('fNameErr')
-
-
-
-    if (namelen == 0) {
-        errorP.innerHTML = 'Please fill the form correctly'
-        errorName1.innerHTML = 'Please fill Your first Name'
-        errorP.style.color = 'red'
+    var errorNameLast1 = document.getElementById('NameErr')
+    var subErr = document.getElementById('subErr')
+    console.log(name1)
+    if (name1.length == 0) {
+        errorName1.innerHTML = 'First Name'
         errorName1.style.color = 'red'
+        errorP.innerHTML = 'Please fill the form correctly'
+        errorP.style.color = 'red'
 
+
+    } else if (name1.length > 0) {
+        errorName1.innerHTML = 'First name'
+        errorName1.style.color = 'Green'
     } else {
+
+        document.querySelector("contactSubmit").action = "page.php";
+    }
+
+    if (lastName1.length == 0) {
+        errorNameLast1.innerHTML = 'ssadadasd'
+        errorNameLast1.style.color = 'red'
+        errorP.innerHTML = 'Please fill the form correctly'
+        errorP.style.color = 'red'
+
+
+    } else if (lastName1.length > 0) {
+        errorNameLast1.innerHTML = 'Last Name'
+        errorNameLast1.style.color = 'Green'
+    } else {
+
+        document.querySelector("contactSubmit").action = "page.php";
+    }
+    if (subject1.length == 0) {
+        subErr.innerHTML = 'ssadadasd'
+        subErr.style.color = 'red'
+        errorP.innerHTML = 'Please fill the form correctly'
+        errorP.style.color = 'red'
+
+    } else if (subject1.length > 0) {
+        subErr.innerHTML = 'Subject'
+        subErr.style.color = 'green'
+    } else {
+        document.querySelector("submit").action = "page.php";
 
     }
     return false
 }
+
 
 /*
 submitB.addEventListener('click', function() {
@@ -39,5 +77,4 @@ submitB.addEventListener('click', function() {
 */
 /*
 function submitContact() {
-    document.getElementById("contactSubmit").action = "page.php";
-}*/
+    document.getElementById("contactSubmit").action = "page.php";*/
